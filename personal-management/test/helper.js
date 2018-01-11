281f4e748d33f2a66ad3c131583d39fa3bc9a2c8
tylde <mr.mkalinowski@gmail.com>
2018-01-11 10:35:44 +0100

    tests integration

--- personal-management/test/helper.js
+++ personal-management/test/helper.js
@@ -2,6 +2,10 @@ import { expect } from 'chai';
 import { sinon, spy } from 'sinon';
 import { mount, render, shallow } from 'enzyme';
 
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
 global.expect = expect;
 global.sinon = sinon;
 global.spy = spy;
