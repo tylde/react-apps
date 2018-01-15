ca5d412d7d2cf22996c26cdd3fdb76991059430d
tylde <mr.mkalinowski@gmail.com>
2018-01-15 16:53:29 +0100

    App.test.js / learning test writing

--- personal-management/src/App/App.test.js
+++ personal-management/src/App/App.test.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import App from './App';
 
+import Header from './Header';
+
+import MenuButton from './NavBar/MenuButton'
+
 describe('App', () => {
+  // const setup = 
+
+
   let wrapper;
 
   beforeEach(() => {
-    wrapper = render(<App />);
+    wrapper = mount(<App />);
   });
 
 
   it('contains Main component', () => {
     expect(wrapper.find('.main-container')).to.have.length(1);
+  });
+  it('contains Header component', () => {
+    expect(wrapper.find('.header-container')).to.have.length(1);
+  });
+  it('contains MenuButton component ', () => {
+    console.log(wrapper.find(MenuButton));
+    // expect(wrapper.find(MenuButton)).to.have.length(1);
   });
   it('contains NavBar component', () => {
     expect(wrapper.find('.navbar-container')).to.have.length(1);
